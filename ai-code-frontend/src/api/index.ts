0edc8bad70ae5be687ef8898bc475398f6f2d020
Joseph <liujingtao529@gmail.com>
2026-04-24 16:35:01 +1000

    execute npm run openapi2ts to gen frontend APIs from backend

--- ai-code-frontend/src/api/index.ts
+++ ai-code-frontend/src/api/index.ts
@@ -1,8 +1,14 @@
+// @ts-ignore
 /* eslint-disable */
-// @ts-nocheck
+// API 更新时间：
+// API 唯一标识：
 import * as usersController from './usersController'
+import * as appController from './appController'
+import * as staticResourceController from './staticResourceController'
 import * as healthController from './healthController'
 export default {
   usersController,
+  appController,
+  staticResourceController,
   healthController,
 }
--- ai-code-frontend/src/api/usersController.ts
+++ ai-code-frontend/src/api/usersController.ts
@@ -1,5 +1,5 @@
+// @ts-ignore
 /* eslint-disable */
-// @ts-nocheck
 import request from '@/request'
 
 /** 此处后端没有提供注释 POST /users/add */
@@ -30,7 +30,7 @@ export async function deleteUser(body: API.DeleteRequest, options?: { [key: stri
 export async function getUserById(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: API.getUserByIdParams,
-  options?: { [key: string]: any },
+  options?: { [key: string]: any }
 ) {
   return request<API.BaseResponseUsers>('/users/get', {
     method: 'GET',
@@ -53,7 +53,7 @@ export async function getLoginUser(options?: { [key: string]: any }) {
 export async function getUserVoById(
   // 叠加生成的Param类型 (非body参数swagger默认没有生成对象)
   params: API.getUserVOByIdParams,
-  options?: { [key: string]: any },
+  options?: { [key: string]: any }
 ) {
   return request<API.BaseResponseUserVO>('/users/get/vo', {
     method: 'GET',
@@ -67,7 +67,7 @@ export async function getUserVoById(
 /** 此处后端没有提供注释 POST /users/list/page/vo */
 export async function listUserVoByPage(
   body: API.UserQueryRequest,
-  options?: { [key: string]: any },
+  options?: { [key: string]: any }
 ) {
   return request<API.BaseResponsePageUserVO>('/users/list/page/vo', {
     method: 'POST',
